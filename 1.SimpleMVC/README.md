14ba20eeffa4fa6b8e6ebc50f9c67eb64f842399
novakvova <novakvova@gmail>
2024-12-04 16:26:34 +0200

    Add static and layout

---
+++ 1.SimpleMVC/README.md
@@ -0,0 +1,10 @@
+py --version
+
+py -m venv .venv
+
+code .
+
+.venv/Scripts/activate.bat
+cd blog
+py manage.py runserver 9589
+
---
+++ 1.SimpleMVC/blog/blog/views.py
@@ -0,0 +1,11 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+
+def homepage(request):
+    return render(request, "home.html")
+    # return HttpResponse("Головна сторінка")
+
+def contactpage(request):
+    return render(request, "contact.html")
+    #return HttpResponse("Контакти")
